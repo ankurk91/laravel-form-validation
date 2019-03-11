f5f460ea1842318a06fe5ed46a61f7f7e57d3a7b
ankurk91 <pro.ankurk1@gmail.com>
2019-03-11 14:47:49 +0530

    Add more tests

--- __test__/Errors.test.js
+++ __test__/Errors.test.js
@@ -1,20 +1,111 @@
 import Errors from '../src/Errors.js';
 
-let errors;
+let $errors;
 
 describe('Errors class', () => {
 
   beforeEach(() => {
-    errors = new Errors();
+    $errors = new Errors();
   });
 
   it('has no errors by default', () => {
-    expect(errors.any()).toBe(false);
-    expect(errors.all()).toEqual({});
+    expect($errors.any()).toBe(false);
+    expect($errors.all()).toEqual({});
   });
 
-  it('can accept errors from constructor', () => {
+  it('can accept errors via constructor', () => {
+    const errors = {
+      name: ['Name is required']
+    };
 
-  })
+    expect((new Errors(errors)).all()).toEqual(errors)
+  });
+
+  it('can accept errors via record()', () => {
+    const errors = {
+      name: ['Name is required']
+    };
+    $errors.record(errors);
+
+    expect($errors.all()).toEqual(errors)
+  });
+
+  it('can clear() all errors', () => {
+    const errors = {
+      name: ['Name is required']
+    };
+    $errors.record(errors);
+    $errors.clear();
+
+    expect($errors.all()).toEqual({})
+  });
+
+  it('can clear() all errors for given field', () => {
+    const errors = {
+      name: ['Name is required'],
+      email: ['Email is required'],
+    };
+    $errors.record(errors);
+    $errors.clear('name');
+
+    expect($errors.has('name')).toBe(false);
+    expect($errors.has('email')).toBe(true);
+    expect($errors.get('email')).toEqual(errors.email);
+  });
+
+  it('can determine if a given field or object has() any errors', () => {
+    expect($errors.any()).toBe(false);
+
+    $errors.record({
+      first_name: ['Field is required'],
+      'person.0.first_name': ['Field is required'],
+    });
+
+    expect($errors.has('first')).toBe(false);
+    expect($errors.has('first_name')).toBe(true);
+    expect($errors.has('person')).toBe(true);
+  });
+
+  it('can get() all errors for given field', () => {
+    const allErrors = {first_name: ['Field is required']};
+
+    $errors.record(allErrors);
+
+    expect($errors.get('first_name')).toEqual(allErrors.first_name);
+    expect($errors.get('unknown_field')).toEqual([]);
+  });
+
+  it('can get first() error for the given field', () => {
+    $errors.record({first_name: ['Field is required']});
+
+    expect($errors.first('first_name')).toEqual('Field is required');
+  });
+
+  it('can clear() all errors of the given key having a dot (.) or [', () => {
+    $errors.record({
+      'profile.first_name': ['Field is required'],
+      'profile.last_name': ['Field is required'],
+      'dates.0.start_date': ['Field is required'],
+      'dates.1.start_date': ['Field is required'],
+      'roles[0].name': ['Field is required'],
+      'roles[1].name': ['Field is required'],
+    });
+
+    $errors.clear('profile');
+    $errors.clear('dates.0');
+    $errors.clear('roles[1]');
+
+    expect($errors.has('profile')).toBe(false);
+    expect($errors.has('profile.first_name')).toBe(false);
+    expect($errors.has('profile.last_name')).toBe(false);
+
+    expect($errors.has('dates')).toBe(true);
+    expect($errors.has('dates.0.start_date')).toBe(false);
+    expect($errors.has('dates.1.start_date')).toBe(true);
+
+    expect($errors.has('roles')).toBe(true);
+    expect($errors.has('roles[0].name')).toBe(true);
+    expect($errors.has('roles[1].name')).toBe(false);
+  });
 
 });
--- __test__/Form.test.js
+++ __test__/Form.test.js
@@ -3,20 +3,137 @@ import Errors from '../src/Errors.js';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-let form;
-let mockAdapter;
+let $form;
+let $mockAdapter;
 
 describe('Form class', () => {
 
   beforeEach(() => {
-    form = new Form();
-    mockAdapter = new MockAdapter(axios);
+    $form = new Form();
+    $mockAdapter = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
   it('has default values', () => {
-    expect(form.$pending).toBe(false);
-    expect(form.$progress).toEqual(0);
-    expect(form.$errors).toBeInstanceOf(Errors);
-  })
+    expect($form.$pending).toBe(false);
+    expect($form.$progress).toEqual(0);
+    expect($form.$errors).toBeInstanceOf(Errors);
+  });
+
+  it('can record the errors sent by server', async () => {
+    $mockAdapter.onPost('http://localhost/signup')
+      .reply(422, {
+        errors: {first_name: ['Field is required']},
+      });
+
+    try {
+      await $form.post('http://localhost/signup');
+    } catch (e) {
+      expect(e.response.status).toBe(422)
+    }
+
+    expect($form.$errors.has('first_name')).toBe(true);
+  });
+
+  it('transforms the data to a FormData object if there is a File', async () => {
+    const file = new File([new Uint8Array(10000)], {type: 'image/png'});
+
+    const data = {
+      email: 'john@example.com',
+      profile: {
+        name: 'John D',
+        roles: ['admin', 'coach'],
+        dob: new Date(Date.UTC(2019, 5, 22, 5, 15)),
+      },
+      avatar: file,
+      level: null
+    };
+
+    $mockAdapter.onPost('http://localhost/profile')
+      .reply(request => {
+        expect(request.data).toBeInstanceOf(FormData);
+        expect(request.data.get('email')).toBe('john@example.com');
+        expect(request.data.get('profile[name]')).toBe('John D');
+        expect(request.data.get('profile[roles][0]')).toBe('admin');
+        expect(request.data.get('profile[roles][1]')).toBe('coach');
+        expect(request.data.get('profile[dob]')).toBe('2019-06-22T05:15:00.000Z');
+        expect(request.data.get('avatar')).toEqual(file);
+
+        // Convert null to empty string
+        expect(request.data.get('level')).toEqual("");
+        // Form method spoofing
+        expect(request.data.get('_method')).toEqual('put');
+
+        expect(request.url).toBe('http://localhost/profile');
+        expect(request.onUploadProgress).toBeInstanceOf(Function);
+
+        return [200, {}];
+      });
+
+    await $form.put('http://localhost/profile', data);
+  });
+
+  it('can detect if object/array has a file deeply', () => {
+    expect($form.hasFile({})).toBe(false);
+
+    expect($form.hasFile({
+      name: 'Foo'
+    })).toBe(false);
+
+    expect($form.hasFile({
+      avatar: new Blob([], {})
+    })).toBe(true);
+
+    expect($form.hasFile({
+      documents: [new File([], {})]
+    })).toBe(true);
+
+    expect($form.hasFile({
+      profile: {avatar: new File([], {})}
+    })).toBe(true);
+  });
+
+  it('can directly call get() method', () => {
+    const stub = jest.fn();
+    $form.submit = stub;
+
+    $form.get("http://localhost", {search: "test"});
+    expect(stub).toHaveBeenCalledWith("get", "http://localhost", {}, {params: {search: "test"}});
+  });
+
+  it('can directly call post() method', () => {
+    const stub = jest.fn();
+    $form.submit = stub;
+
+    $form.post("http://localhost", {foo: "bar"});
+    expect(stub).toHaveBeenCalledWith("post", "http://localhost", {foo: "bar"});
+  });
+
+  it('can directly call put() method', () => {
+    const stub = jest.fn();
+    $form.submit = stub;
+
+    $form.put("http://localhost", {name: "test"});
+    expect(stub).toHaveBeenCalledWith("put", "http://localhost", {name: "test"});
+  });
+
+  it('can directly call patch() method', () => {
+    const stub = jest.fn();
+    $form.submit = stub;
+
+    $form.patch("http://localhost", {name: "test"});
+    expect(stub).toHaveBeenCalledWith("patch", "http://localhost", {name: "test"});
+  });
+
+  it('can directly call delete() method', () => {
+    const stub = jest.fn();
+    $form.submit = stub;
+
+    $form.delete("http://localhost", {slug: "test"});
+    expect(stub).toHaveBeenCalledWith("delete", "http://localhost", {slug: "test"});
+  });
 
 });
